c9535f2ff8867ba3ad3c71ec13670ffd2a4cabc9
Oleg Sucharevich <olegs@codefresh.io>
2019-12-15 22:48:45 +0200

    use alpine based image to build services

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -2,9 +2,12 @@ echo "Generating temp file for $NAME"
 cd $NAME
 go generate scripts/generate.go
 VERSION=$(cat ../VERSION)
+
 echo "Building $NAME-$VERSION service fro darwin and amd64"
 GOOS=darwin GOARCH=amd64 go build -o ../dist/$NAME-$VERSION-darwin-amd64 .
+
 echo "Building $NAME-$VERSION service fro linux and amd64"
 GOOS=linux GOARCH=amd64 go build -o ../dist/$NAME-$VERSION-linux-amd64 .
+
 echo "Building $NAME-$VERSION service fro linux and 386"
 GOOS=linux GOARCH=386 go build -o ../dist/$NAME-$VERSION-linux-386 .
