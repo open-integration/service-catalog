247c9b20d2f310d345ecc62b5be2926dbd5ac857
Oleg Sucharevich <olegs@codefresh.io>
2019-12-24 14:49:28 +0200

    push git tag for each new serivce release

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -6,6 +6,25 @@ for s in $SERVICES
 do  
     echo "Building service $s"
     cd $s
-    make build
+    # make build
+    VERSION=$(cat ./VERSION)
+    GO_MOD_CANDIDATE=$s/v$VERSION
+    LIST=$(git tag --list)
+    ALREADY_EXISTS="false"
+    for t in $LIST
+    do
+        if [ $GO_MOD_CANDIDATE == $t ]
+        then
+            echo "$GO_MOD_CANDIDATE == $t skipping new release for $s"
+            ALREADY_EXISTS="true"
+        fi
+    done
+    
+    if [ $ALREADY_EXISTS == "false" ]
+    then
+        echo "Creating new git tag $GO_MOD_CANDIDATE"
+        git tag $GO_MOD_CANDIDATE
+        git push --tags
+    fi
     cd ../
 done
